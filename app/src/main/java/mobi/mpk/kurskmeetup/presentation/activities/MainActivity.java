83daa6936613a638b7d8a75bb6370662cce03f2e
Alexander Zuev <zuev08@gmail.com>
2016-04-17 19:34:57 +0300

    Добавлен Crashlytics

--- app/src/main/java/mobi/mpk/kurskmeetup/presentation/activities/MainActivity.java
+++ app/src/main/java/mobi/mpk/kurskmeetup/presentation/activities/MainActivity.java
@@ -7,6 +7,9 @@ import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 
+import com.crashlytics.android.Crashlytics;
+import io.fabric.sdk.android.Fabric;
+
 import mobi.mpk.kurskmeetup.R;
 import mobi.mpk.kurskmeetup.presentation.adapters.TabsPagerAdapter;
 
@@ -30,6 +33,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Fabric.with(this, new Crashlytics());
         setContentView(R.layout.activity_main);
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
