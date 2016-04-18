171435869e15101055a94fa0d8639b8536ca1ae6
Alexander Zuev <zuev08@gmail.com>
2016-04-18 23:10:00 +0300

    Добавлены Unit тесты

---
+++ app/src/test/java/mobi/mpk/kurskmeetup/application/ApiMeetupsServiceTest.java
@@ -0,0 +1,84 @@
+package mobi.mpk.kurskmeetup.application;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.lang.reflect.Field;
+import java.util.List;
+
+import mobi.mpk.kurskmeetup.data.AsyncRepository;
+import mobi.mpk.kurskmeetup.data.OnDataLoadListener;
+import mobi.mpk.kurskmeetup.domain.MeetupsService;
+import mobi.mpk.kurskmeetup.domain.dto.Meetup;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+
+public class ApiMeetupsServiceTest {
+    private AsyncRepository repository;
+    private MeetupsService service;
+
+    @Before
+    public void init() {
+        repository = mock(AsyncRepository.class);
+        service = ApiMeetupsService.getInstance(repository);
+    }
+
+    @Test
+    public void testGetMeetups() throws Exception {
+
+        service.getMeetups();
+
+        verify(repository).getMeetups(any(OnDataLoadListener.class));
+    }
+
+    @Test
+    public void testRegisterObserver() throws Exception {
+        OnDataLoadListener listener = new OnDataLoadListener<List<Meetup>>() {
+            @Override
+            public void onSuccess(List<Meetup> data) {
+
+            }
+
+            @Override
+            public void onFailure(Throwable throwable) {
+
+            }
+        };
+
+        service.registerObserver(listener);
+
+        Field observersList = service.getClass().getDeclaredField("observers");
+        observersList.setAccessible(true);
+        List<OnDataLoadListener<List<Meetup>>> observers =
+                (List<OnDataLoadListener<List<Meetup>>>) observersList.get(service);
+        assertTrue(observers.contains(listener));
+    }
+
+    @Test
+    public void testUnregisterObserver() throws Exception {
+        OnDataLoadListener listener = new OnDataLoadListener() {
+            @Override
+            public void onSuccess(Object data) {
+
+            }
+
+            @Override
+            public void onFailure(Throwable throwable) {
+
+            }
+        };
+
+        service.registerObserver(listener);
+        service.unregisterObserver(listener);
+
+        Field observersList = service.getClass().getDeclaredField("observers");
+        observersList.setAccessible(true);
+        List<OnDataLoadListener<List<Meetup>>> observers =
+                (List<OnDataLoadListener<List<Meetup>>>) observersList.get(service);
+        assertFalse(observers.contains(listener));
+    }
+}
